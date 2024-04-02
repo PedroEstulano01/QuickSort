bb4d6d3091606fb36860a27aa8e4ec80cb839335
Estul4z <108631250+PedroEstulano01@users.noreply.github.com>
2024-04-02 20:11:08 -0300

    Update QuickSort.java

--- QuickSort/src/main/java/com/mycompany/quicksort/QuickSort.java
+++ QuickSort/src/main/java/com/mycompany/quicksort/QuickSort.java
@@ -3,46 +3,46 @@ package com.mycompany.quicksort;
 
 public class QuickSort {
 
-    public static void quickSort(int[] numeros, int inicio, int fim) {
+    public static void quickSort(int[] vet, int inicio, int fim) {
         if (inicio < fim) {
-            int posicaoPivo = particionar(numeros, inicio, fim);
-            quickSort(numeros, inicio, posicaoPivo - 1);
-            quickSort(numeros, posicaoPivo + 1, fim);
+            int posicaoPivo = particionar(vet, inicio, fim);
+            quickSort(vet, inicio, posicaoPivo - 1);
+            quickSort(vet, posicaoPivo + 1, fim);
         }
     }
 
-    private static int particionar(int[] numeros, int inicio, int fim) {
-        int pivo = numeros[fim];
+    private static int particionar(int[] vet, int inicio, int fim) {
+        int pivo = vet[fim];
         int i = inicio - 1;
 
         for (int j = inicio; j < fim; j++) {
-            if (numeros[j] <= pivo) {
+            if (vet[j] <= pivo) {
                 i++;
-                trocar(numeros, i, j);
+                trocar(vet, i, j);
             }
         }
 
-        trocar(numeros, i + 1, fim);
+        trocar(vet, i + 1, fim);
         return i + 1;
     }
 
-     private static void trocar(int[] numeros, int i, int j) {
-    int temp = numeros[i];
-    numeros[i] = numeros[j];
-    numeros[j] = temp;
+     private static void trocar(int[] vet, int i, int j) {
+    int temp = vet[i];
+    vet[i] = vet[j];
+    vet[j] = temp;
     }
 
     public static void main(String[] args) {
-        int[] numeros = {20, 16, 15, 10, 9, 8, 7, 5, 1, 0};
-        quickSort(numeros, 0, numeros.length - 1);
+        int[] vet = {20, 16, 15, 10, 9, 8, 7, 5, 1, 0};
+        quickSort(vet, 0, vet.length - 1);
 
-        System.out.println("Array desordenado: ");
-        for (int i : numeros) {
+        System.out.println("Vetor desordenado: ");
+        for (int i : vet) {
             System.out.print(i + " ");
         }
         
-        System.out.println("\nArray ordenado: ");
-        for (int i : numeros) {
+        System.out.println("\n Vetor ordenado: ");
+        for (int i : vet) {
             System.out.print(i + " ");
         }
     }
